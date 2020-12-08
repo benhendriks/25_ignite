86e7fb771b48d0a363b4f20faa25e773bc90503d
Benjamin Steidl <localartpool@gmail.com>
2020-12-08 16:24:09 +0100

    Styled game details

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 const Detail = styled(motion.div)`
   width: 80%;
   border-radius: 1rem;
-  padding: 2rem 20rem;
+  padding: 2rem 5rem;
   background: #fff;
   position: absolute;
   left: 10%;
@@ -29,11 +29,39 @@ const CardShadow = styled(motion.div)`
   }
   &::-webkit-scrollbar-thumb {
     background-color: #0037ff;
-
   }
   &::-webkit-scrollbar-track {
     background: #fff;   
   }
+`;
+const Stats = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled(motion.div)`
+  text-align: center;
+`;
+
+const Platforms = styled(motion.div)`
+  display: flex;
+  justify-content: space-evenly;
+  img {
+    margin-left: 3rem; 
+  }
+`;
+
+const Media = styled(motion.div)`
+  margin-top: 5rem;
+  img {
+    width: 100%;
+  } 
+`;
+
+const Description = styled(motion.div)`
+  margin: 5rem 0rem;
+
 `;
 
 const GameDetail = () => {
@@ -41,28 +69,28 @@ const GameDetail = () => {
   return (
     <CardShadow>
       <Detail>
-        <div className="stats">
+        <Stats>
           <div className="rating">
             <h3>{game.name}</h3>
               <p>Rating: {game.rating}</p>
           </div>
-          <div className="info">
+          <Info>
             <h3>Platforms</h3>
-            <div className="platforms">
+            <Platforms>
               {game.platforms && game.platforms.map(data => (
                 <h3 key={data.platform.id}>
                   {data.platform.name}
                 </h3>
               ))}
-            </div>
-          </div>
-        </div>
-        <div className="media">
+            </Platforms>
+          </Info>
+        </Stats>
+        <Media>
           <img src={game.background_image} alt={game.background_image}/>
-        </div>
-        <div className="description">
+        </Media>
+        <Description>
           <p>{game.description_raw}</p>
-        </div>
+        </Description>
         <div className="gallary">
           {screen.results && screen.results.map(screen => (
             <img src={screen.image} key={screen.id} alt={screen.image}/>
