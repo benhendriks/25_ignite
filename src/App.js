5ed43e222704d3ff32c5e2d2ab026a4ee09dab2e
Benjamin Steidl <localartpool@gmail.com>
2020-12-08 11:00:43 +0100

    Worked on global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyle from './components/GlobalStyles';
 import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <GlobalStyle />
+        <Home />
     </div>
   );
 }
